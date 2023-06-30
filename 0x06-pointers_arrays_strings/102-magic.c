0522b0184d39f760bcdcc814f841a111b2af358d
hichamassri <hichamassri17@gmail.com>
2023-06-29 17:32:00 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 
+/**
+ * The main function of the program.
+ * It demonstrates a magic trick by indirectly modifying a[2] to be 98.
+ * Prints the value of a[2] before and after the modification.
+ */
 int main(void)
 {
-int n;
-int a[5];
-int *p;
+    int n;
+    int a[5];
+    int *p;
 
-a[2] = 1024;
-p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
-*(p + 5) = 98;
-  /* ...so that this prints 98\n */
-printf("a[2] = %d\n", a[2]);
-return (0);
+    a[2] = 1024;
+    p = &n;
+    /*
+     * Write your line of code here...
+     * Remember:
+     * - You are not allowed to use 'a'
+     * - You are not allowed to modify 'p'
+     * - Only one statement
+     * - You are not allowed to code anything else than this line of code
+     */
+    printf("a[2] = %d\n", *(p + 7));
+    *(p + 5) = 98; /* Indirectly modify a[2] to be 98 */
+    /* ...so that this prints 98\n */
+    printf("a[2] = %d\n", a[2]);
+    return 0;
 }
